eb357f52bab4ab9ab0a5914fee3a661fa8ba1e82
Deborah <86526328+dehscardoso@users.noreply.github.com>
2021-11-12 09:44:20 -0300

    add queries

--- Databases_and_SQL_for_Data_Science_with_Python/Week 1/Hands-on Lab - Simple SELECT Statements.sql
+++ Databases_and_SQL_for_Data_Science_with_Python/Week 1/Hands-on Lab - Simple SELECT Statements.sql
@@ -1,56 +1,41 @@
 -- Retrieve all records with all columns from the "FilmLocations" table.
-SELECT
-    Title,
+SELECT Title,
     Director,
     Writer
-FROM
-    FilmLocations;
+FROM FilmLocations;
 
 -- Retrieve the names of all films with director names and writer names.
-SELECT
-    Title,
+SELECT Title,
     Director,
     Writer
-FROM
-    FilmLocations;
+FROM FilmLocations;
 
 -- Retrieve the names of all films released in the 21st century and onwards 
 -- (release years after 2001 including 2001), along with filming locations and release years.
-SELECT
-    Title,
+SELECT Title,
     ReleaseYear,
     Locations
-FROM
-    FilmLocations
-WHERE
-    ReleaseYear >= 2001;
+FROM FilmLocations
+WHERE ReleaseYear >= 2001;
 
 -- Retrieve the fun facts and filming locations of all films.
-SELECT
-    Locations,
+SELECT Locations,
     FunFacts
-FROM
-    FilmLocations;
+FROM FilmLocations;
 
 -- Retrieve the names of all films released in the 20th century and before (release years 
 -- before 2000 including 2000) that, along with filming locations and release years.
-SELECT
-    Title,
+SELECT Title,
     ReleaseYear,
     Locations
-FROM
-    FilmLocations
-WHERE
-    ReleaseYear <= 2000;
+FROM FilmLocations
+WHERE ReleaseYear <= 2000;
 
 -- Retrieve the names, production company names, filming locations, and release years 
 -- of the films which are not written by James Cameron.
-SELECT
-    Title,
+SELECT Title,
     ProductionCompany,
     Locations,
     ReleaseYear
-FROM
-    FilmLocations
-WHERE
-    Writer <> "James Cameron";
+FROM FilmLocations
+WHERE Writer <> "James Cameron";
